6813d834d46a5d14e9103ce61299018e0354a2f2
Vladimir Kononovich <vova.kononovich.main@gmail.com>
2023-11-11 16:21:36 +0300

    Add canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const canvasRef = useRef(null);
+  let canvas: HTMLCanvasElement;
+  let ctx;
+  let storage: [][] | null = null;
+  const [rectSize, setRectSize] = useState(50);
+
+  useEffect(() => {
+    canvas = canvasRef.current!;
+    canvas.height = window.innerHeight - (window.innerHeight % rectSize);
+    canvas.width = window.innerWidth;
+    ctx = canvas.getContext("2d");
+
+    storage = Array.from(new Array(canvas.height / rectSize), () => {
+      return [];
+    });
+
+  });
+
   return (
     <div className="App">
-      
+      <canvas id="canvas" ref={canvasRef}></canvas>
     </div>
   );
 }
